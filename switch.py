ab6bb5c81d5a6f887b35983c6e5ddfaf04f86a5d
sachinpatelx@gmail.com <sachinpatelx@gmail.com>
2017-02-26 22:27:12 -0500

    switch py

--- switch.py
+++ switch.py
@@ -2,12 +2,21 @@ import time
 import RPi.GPIO as io
 io.setmode(io.BCM)
 
-door_pin = 2
-x = True
-io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
+reed_switch_pin = 2
+startdisplay = False
+
+io.setup(reed_switch_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
+
+
 
 while True:
-    x = not x
-    if not io.input(door_pin):
-        print("Reed switch detected 1" )
-    time.sleep(1)
+    pedals = 0
+    now = time.time()
+    future = now + 10
+    while(time.time() <= future)
+        if not io.input(reed_switch_pin):
+            ++pedals
+            print("Reed switch detected")
+        if pedals >= 5:
+            print("Starting display...")
+            return True
