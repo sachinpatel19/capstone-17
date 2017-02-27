b1f3ae0d6f73644efdf842e9b50a2cce0fd4e6e9
sachinpatelx@gmail.com <sachinpatelx@gmail.com>
2017-02-26 21:25:50 -0500

    switch py

--- switch.py
+++ switch.py
@@ -3,10 +3,14 @@ import RPi.GPIO as io
 io.setmode(io.BCM)
 
 door_pin = 2
-
+x = True
 io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
 
 while True:
+    x = not x
     if io.input(door_pin):
-        print("Reed switch detected")
+        if(x = True)
+            print("Reed switch detected 1" )
+        else
+            print("Reed switch detected 2" )
     time.sleep(1)
