fff730a1828bc8c657d8c77355d4e0dba46cb935
sachinpatelx@gmail.com <sachinpatelx@gmail.com>
2017-02-26 21:37:56 -0500

    switch py

--- switch.py
+++ switch.py
@@ -8,6 +8,6 @@ io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
 
 while True:
     x = not x
-    if io.input(door_pin):
+    if not io.input(door_pin):
         print("Reed switch detected 1" )
     time.sleep(1)
