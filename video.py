5a964a452b519cbfa368acef6fdb64f71901214e
Harsh Patel <sachinpatelx@gmail.com>
2017-03-20 21:18:19 -0400

    time

--- video.py
+++ video.py
@@ -3,9 +3,8 @@ import os
 from subprocess import Popen
 
 movie1 = ("/home/pi/Videos/movie1.mp4")
-
-
-
+now = time.time()
+future = now + 10
 while True:
     os.system('killall omxplayer.bin')
     omxc = Popen(['omxplayer', '-b', movie1])
