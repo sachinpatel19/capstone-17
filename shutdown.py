7ede0465f9e353d4c9a85ab63227fa7a549f14d2
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-04 22:12:56 -0400

    Create shutdown.py
    shutdown pi

---
+++ shutdown.py
@@ -0,0 +1,12 @@
+# Import the libraries to use time delays, send os commands and access GPIO pins
+import RPi.GPIO as GPIO
+import time
+import os
+
+GPIO.setmode(GPIO.BOARD) # Set pin numbering to board numbering
+GPIO.setup(7, GPIO.IN) # Setup pin 7 as an input
+while True: # Setup a while loop to wait for a button press
+   if(GPIO.input(7)): # Setup an if loop to run a shutdown command when button press sensed
+   os.system("sudo shutdown -h now") # Send shutdown command to os
+    break
+   time.sleep(1) # Allow a sleep time of 1 second to reduce CPU usage
