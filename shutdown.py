bba251214f8f12d08539208d88db2488d71cd111
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-04 22:29:16 -0400

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -2,6 +2,8 @@
 import RPi.GPIO as GPIO
 import time
 import os
+
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.setmode(GPIO.BOARD) # Set pin numbering to board numbering
 #GPIO.setup(10, GPIO.IN) # Setup pin 7 as an input
