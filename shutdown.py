2ee762adf671a5b562bb4e0416a689fc5d077e6d
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-04 22:18:09 -0400

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -4,9 +4,9 @@ import time
 import os
 
 GPIO.setmode(GPIO.BOARD) # Set pin numbering to board numbering
-GPIO.setup(11, GPIO.IN) # Setup pin 7 as an input
+GPIO.setup(10, GPIO.IN) # Setup pin 7 as an input
 while True: # Setup a while loop to wait for a button press
-   if(GPIO.input(11)): # Setup an if loop to run a shutdown command when button press sensed
+   if(GPIO.input(10)): # Setup an if loop to run a shutdown command when button press sensed
       os.system("sudo shutdown -h now") # Send shutdown command to os
       break
    time.sleep(1) # Allow a sleep time of 1 second to reduce CPU usage
