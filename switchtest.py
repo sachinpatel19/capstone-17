53943e91aaf973e4d3a0bb8f8a671b2ca206ad68
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-08 15:12:32 -0400

    Create switchtest.py

---
+++ switchtest.py
@@ -0,0 +1,16 @@
+# Import the libraries to use time delays, send os commands and access GPIO pins
+import RPi.GPIO as GPIO
+import time
+import os
+
+shutdown_switch_pin=17 #board pin = 11
+#This is to keep the raspberry pi on 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(shutdown_switch_pin)
+
+while True: # Setup a while loop to wait for a button press
+   if(GPIO.input(shutdown_switch_pin)): # Setup an if loop to run a shutdown command when button press sensed
+      print("BUTTON PRESSED")
+      #time.sleep(3)
+      break
+   time.sleep(1) # Allow a sleep time of 1 second to reduce CPU usage
