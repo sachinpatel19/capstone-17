1c37e8bf56ffb454f3aa2c9b72e8275b0a5a7352
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-08 14:49:49 -0400

    updating how things work

--- shutdown.py
+++ shutdown.py
@@ -3,13 +3,17 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN)
-#GPIO.setmode(GPIO.BOARD) # Set pin numbering to board numbering
-#GPIO.setup(10, GPIO.IN) # Setup pin 7 as an input
+power_on_pin=4 #board pin = 7
+shutdown_switch_pin=17 #board pin = 11
+#This is to keep the raspberry pi on 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(power_on_pin,GPIO.OUT)
+GPIO.output(power_on_pin,GPIO.HIGH)
+
+GPIO.setup(shutdown_switch_pin, GPIO.IN)
 
 while True: # Setup a while loop to wait for a button press
-   if(GPIO.input(10)): # Setup an if loop to run a shutdown command when button press sensed
+   if(GPIO.input(shutdown_switch_pin)): # Setup an if loop to run a shutdown command when button press sensed
       print("shutdown will initiate in 3..2..1..")
       #time.sleep(3)
       os.system("sudo shutdown -h now") # Send shutdown command to os
