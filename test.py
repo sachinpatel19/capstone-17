f9d8107d311b8e3d5250a9f8e87efde267725402
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-17 23:30:24 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-power_on_pin=2 #bcm
+power_on_pin=4 #bcm
 #This is to keep the raspberry pi on 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(power_on_pin,GPIO.OUT)
