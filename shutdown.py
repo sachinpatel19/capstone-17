bd722bb782116a7f39320cd396c6397a7950da5f
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-17 23:58:12 -0400

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -3,8 +3,8 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-power_on_pin=2 #board pin = 7
-shutdown_switch_pin=3 #board pin = 11
+power_on_pin=14 #board pin = 7
+shutdown_switch_pin=15 #board pin = 11
 #This is to keep the raspberry pi on 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(power_on_pin,GPIO.OUT)
