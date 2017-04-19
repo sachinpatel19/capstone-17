e6f4496eac18beaaa39e861cb7a41dbe9c3d863d
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-18 21:38:05 -0400

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -2,7 +2,7 @@
 import RPi.GPIO as GPIO
 import time
 import os
-GPIO.cleanup()
+#GPIO.cleanup()
 power_on_pin=14 #board pin = 7
 #shutdown_switch_pin=17 #board pin = 11
 #This is to keep the raspberry pi on 
