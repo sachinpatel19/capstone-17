ae6921a12f4ff4aa1ac10ab8beb515ae177be284
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-18 21:50:03 -0400

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -4,7 +4,7 @@ import time
 import os
 #GPIO.cleanup()
 power_on_pin=4 #board pin = 7
-#shutdown_switch_pin=17 #board pin = 11
+shutdown_switch_pin=14 #board pin = 11
 #This is to keep the raspberry pi on 
 GPIO.setmode(GPIO.BCM)
 #print("setting gpio pin to high\n")
@@ -12,17 +12,15 @@ GPIO.setup(power_on_pin,GPIO.OUT)
 GPIO.output(power_on_pin,GPIO.HIGH)
 
 #print("setting gpio pin to input\n")
-#GPIO.setup(shutdown_switch_pin, GPIO.IN)
+GPIO.setup(shutdown_switch_pin, GPIO.IN)
 #time.sleep(1)
-#while True: # Setup a while loop to wait for a button press
-   #GPIO.output(power_on_pin,GPIO.HIGH)
-   
-   #if GPIO.input(shutdown_switch_pin): # Setup an if loop to run a shutdown command when button press sensed
+while True: # Setup a while loop to wait for a button press
+   if GPIO.input(shutdown_switch_pin): # Setup an if loop to run a shutdown command when button press sensed
       #GPIO.output(power_on_pin,GPIO.LOW)
       #print("shuting down in 2...1...")
       #time.sleep(2)
       #GPIO.cleanup()
-      #print("shuting down")
+      print("shuting down")
       # os.system("sudo shutdown -h now") # Send shutdown command to os
       #break
       #time.sleep(1) # Allow a sleep time of 1 second to reduce CPU usage
