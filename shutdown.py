b24340b4b00e7957da948f0a847cd4cfd51d241f
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-18 21:52:07 -0400

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -16,11 +16,12 @@ GPIO.setup(shutdown_switch_pin, GPIO.IN)
 #time.sleep(1)
 while True: # Setup a while loop to wait for a button press
    if not GPIO.input(shutdown_switch_pin): # Setup an if loop to run a shutdown command when button press sensed
+      print("pressed")
       #GPIO.output(power_on_pin,GPIO.LOW)
       #print("shuting down in 2...1...")
       #time.sleep(2)
       #GPIO.cleanup()
-      print("shuting down")
+      
       # os.system("sudo shutdown -h now") # Send shutdown command to os
       #break
       #time.sleep(1) # Allow a sleep time of 1 second to reduce CPU usage
