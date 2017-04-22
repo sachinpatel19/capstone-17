14b88633c50261604607dd16b35edfd0e05c1252
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-21 23:36:34 -0400

    Update integrationtest.py

--- integrationtest.py
+++ integrationtest.py
@@ -2,27 +2,41 @@
 import time
 import sys
 import RPi.GPIO as io
+from omxplayer import OMXPlayer
 
-io.setmode(io.BCM)
+#movie1 = ("/home/pi/Videos/movie1.mp4")
 
-reed_switch_pin = 26
+reed_switch_pin = 26 # board pin 39 , bcm 26
 startdisplay = False
 
+io.setmode(io.BCM)
 io.setup(reed_switch_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
 
 while True:
     pedals = 0
     now = time.time()
-    future = now + 10
+    future = now + 15
     while time.time() <= future:
         if not io.input(reed_switch_pin):
             pedals += 1
             print("Number of pedals " + str(pedals))
             time.sleep(0.5)
+            # todo: if reed switch continuously on, don't do anything
         if pedals >= 5:
             startdisplay = True
+        else:
+            startdisplay = False
             break
     if startdisplay:
         print("Starting video...")
-        sys.exit(0)
+        os.system('killall omxplayer.bin')
+        omxc = Popen(['omxplayer', '-b', movie1])
+    #else:
+        
+    # if every 15 second, 5 revolution, then keep it on other wise kill it
+    # after 5 revolution reset the time
+    
+    
+    # shutdown system after 5 minutes, shut down the pi 
+    
 
