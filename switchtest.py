33969e02d9ee927f1262815d6d940932986bf20c
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-21 22:39:54 -0400

    Update switchtest.py

--- switchtest.py
+++ switchtest.py
@@ -3,13 +3,13 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-shutdown_switch_pin=4 #board pin = 11
+shutdown_switch_pin=4 #board pin = 7
 #This is to keep the raspberry pi on 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(shutdown_switch_pin,GPIO.IN)
 
 while True: # Setup a while loop to wait for a button press
-   if GPIO.input(shutdown_switch_pin): # Setup an if loop to run a shutdown command when button press sensed
+   if not GPIO.input(shutdown_switch_pin): # Setup an if loop to run a shutdown command when button press sensed
       print("BUTTON PRESSED")
       #time.sleep(3)
       break
