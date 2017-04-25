ca8b4ac357d7a39a720f9b9a736457cb4c81cc31
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-24 21:13:34 -0400

    Update subprocesstest.py

--- subprocesstest.py
+++ subprocesstest.py
@@ -5,7 +5,9 @@ subprocess.call(['rm','./pipeline'])
 subprocess.call(['chmod', '+x', 'mkfifoscript.sh'])
 subprocess.call(['./mkfifoscript.sh'])
 
+print "opening file"
 target = open("pipeline", 'w')
 #subprocess.call(['echo', '-n', '/home/pi/Videos/movie1.mp4', '>', './pipeline'])
+print "writing to file"
 target.write("/home/pi/Videos/movie1.mp4")
 
