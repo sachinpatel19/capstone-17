e0782bf080957741534138b8ac62ffe6e23a1b8d
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-24 21:26:54 -0400

    Update subprocesstest.py

--- subprocesstest.py
+++ subprocesstest.py
@@ -1,13 +1,18 @@
+import os
 import subprocess
 from sys import argv
 
+os.mkfifo("pipeline")
+
 subprocess.call(['rm','./pipeline'])
 subprocess.call(['chmod', '+x', 'mkfifoscript.sh'])
-subprocess.call(['./mkfifoscript.sh'])
+#subprocess.call(['./mkfifoscript.sh'])
+subprocess.call(['omxplayer', '-o', 'hdmi', '"$*"', '<' './pipeline', '&'])
 
 print "opening file"
 target = open("pipeline", 'w')
 #subprocess.call(['echo', '-n', '/home/pi/Videos/movie1.mp4', '>', './pipeline'])
 print "writing to file"
 target.write("/home/pi/Videos/movie1.mp4")
+
 
