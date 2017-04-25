e22afde9f362dff5ce7959494a95cdc2e91bf872
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-24 20:58:31 -0400

    Update subprocesstest.py

--- subprocesstest.py
+++ subprocesstest.py
@@ -1,5 +1,5 @@
 import subprocess
-subprocess.call(['rm -f pipeline'])
+subprocess.call(['rm -f ./pipeline'])
 subprocess.call(['chmod +x mkfifoscript.sh'])
 subprocess.call(['./mkfifoscript.sh'])
 subprocess.call(['echo -n /home/pi/Videos/movie1.mp4 > pipeline'])
