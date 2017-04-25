a6e4f28af7b003b3ce3ab4ae412454ccfffc55b0
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-24 22:07:03 -0400

    Update onemoretest.py

--- onemoretest.py
+++ onemoretest.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
 
-myprocess = subprocess.Popen(['omxplayer','-b','/home/pi/Video/movie1.mp4'],stdin=subprocess.PIPE)
+myprocess = subprocess.Popen(['omxplayer','-b','/home/pi/Videos/movie1.mp4'],stdin=subprocess.PIPE)
 time.sleep(10)
 myprocess.stdin.write('q')
