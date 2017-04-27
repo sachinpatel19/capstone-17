8fe720275f6209228cae7994ec1797023aa8c941
sachinpatel19 <sachinpatelx@gmail.com>
2017-04-26 21:27:50 -0400

    Update integrationtest.py

--- integrationtest.py
+++ integrationtest.py
@@ -33,15 +33,13 @@ while True:
     # set the variable now to be used for checking pedals
     now = time.time()
     future = now + 10
-    
+            #check if button was pressed to shutdown
+    if io.input(shutdown_switch_pin): # Setup an if loop to run a shutdown command when button press sensed
+         print("pressed")
+         io.cleanup()
+         os.system("sudo shutdown -h now") # Send shutdown command to os
     # if now time is less than or equal to future
     while time.time() <= future:
-        #check if button was pressed to shutdown
-        if io.input(shutdown_switch_pin): # Setup an if loop to run a shutdown command when button press sensed
-            print("pressed")
-            io.cleanup()
-            os.system("sudo shutdown -h now") # Send shutdown command to os
-        
         # increment pedals for each input to reed switch
         input = not io.input(reed_switch_pin)
         if (input):
